b6933fc3f68f011002afefad0503e2f993736681
homework <ian061504@gmail.com>
2016-11-29 22:27:06 +0800

    s

--- src/fcu/iecs/oop/pokemon/main.java
+++ src/fcu/iecs/oop/pokemon/main.java
@@ -6,10 +6,6 @@ public class main {
 		Pokemon p1 = new Pokemon("aaa",50);
 		Pokemon p2 = new Pokemon("bbb",80);
 		GYM.fight(p1,p2);
-		Pokemon p3 = new Pokemon("ccc",590);
-		GYM.fight(p1, p3);
-		int a = p3.get_cp();
-		System.out.print(a);
 	}
 
 	
